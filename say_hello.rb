20d8b52c9213cf450fe13fb014a49a54e8575fc1
rghanimah <rabiaghanimah@gmail.com>
2018-08-07 16:00:31 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,5 +2,7 @@
 
 puts "Hello #{your_name}"
 def say_hello (your_name = "Ruby Programmer")
-#{say hello}
+"#{say_hello}"
 end 
+def say_hello (your_name = "Ruby Programmer")
+puts "Hello #{your_name}"
